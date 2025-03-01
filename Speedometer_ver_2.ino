bf3749c969133ceda1aab6a85736c5c29420998c
RJW496 <132957114+RJW496@users.noreply.github.com>
2025-03-01 09:10:56 -0600

    Update Speedometer_ver_2.ino

--- Speedometer_ver_2.ino
+++ Speedometer_ver_2.ino
@@ -45,7 +45,7 @@ float constant;
 #include <LiquidCrystal.h>
 // Arduino pin assignment
 const int rs=12, en=11, d4=5, d5=4, d6=3, d7=2;
-LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+LiquidCrystal lcd(rs ,en, d4, d5, d6, d7);
 
 void setup() {
   lcd.begin(16,2);
